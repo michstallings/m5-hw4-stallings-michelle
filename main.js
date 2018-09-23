0412d57db7ceee6c2fe88e26028782bd9dc0364f
michstallings <contact@michellestallings.com>
2018-09-23 12:27:10 -0700

    Added numContact variables and fixed var num

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-var num = "4"
+var num = 4
 console.log(num)
 
 var numAddition = 10 + 2
 console.log(numAddition)
+
+var numContactOne = 6
+var numContactTwo = "4"
+
+console.log(numContactOne + numContactTwo)
